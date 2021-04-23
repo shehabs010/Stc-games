d1180a133e93934ed01b111dbb84c155262a57a0
shehabs010 <shehabs010@gmail.com>
2021-04-23 02:43:22 +0200

    Update servicesRequests.js

--- src/servicesRequests.js
+++ src/servicesRequests.js
@@ -5,10 +5,10 @@ const domain = 'https://api.rawg.io/api';
 
 // this request take Term or any text to search by it and return result
 
-export const getResultItems = () => http.get(`${domain}/games`);
+export const getResultItems = () => http.get(`${domain}/games?key=efc6929f54b0472993f5e6f517794d4e`);
 
 // this request get all tags
-export const getTags = () => http.get(`${domain}/tags`);
+export const getTags = () => http.get(`${domain}/tags?key=efc6929f54b0472993f5e6f517794d4e`);
 
 
 // this request take nextPageToken to get newest Items from next Pages
